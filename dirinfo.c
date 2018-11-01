a9351e2490d564a2ffdf3cfc6cb14431f5c43d8d
ColderWinters <mwinter@stuy.edu>
2018-11-01 11:42:59 -0400

    removed a bunch of stuff and renamed the file to work

--- dirinfo.c
+++ dirinfo.c
@@ -1,5 +1,25 @@
-struct file{char* name;char*mode;int permissions [4];};
+void olist(){
+
+
+}
+
+void rlist(){
+  
+}
+
+void dlist(){
+
+
+}
+
+int fsize(){
+
+  return 0;
+}
 
 int main(){
+  printf("Statistics for directory: \n");
+  printf("Total Directory Size:%i Bytes \n", fsize());
+  olist();
   return 0;
 }
