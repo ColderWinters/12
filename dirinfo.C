6f84b5eb58358675a01592584af5d19c76dfa63a
SararAseer <saseer@stuy.edu>
2018-11-01 11:39:22 -0400

    basic functions & main template

---
+++ dirinfo.C
@@ -0,0 +1,24 @@
+void olist(){
+
+
+}
+
+void rlist(){
+  
+}
+
+void dlist(){
+
+
+}
+
+int fsize(){
+  return 0;
+}
+
+int main(){
+  printf("Statistics for directory: \n");
+  printf("Total Directory Size:%i Bytes \n", fsize());
+  olist();
+  return 0;
+}
