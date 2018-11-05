332e6306f608b006949b7b81d9650205d8080adb
Sarar Aseer <saseer@stuy.edu>
2018-11-04 20:45:52 -0500

    List files, and size

--- dirinfo.c
+++ dirinfo.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <sys/stat.h>
 
-int main(int argc, char *argv[] ){
 
-  DIR directory;
+int main(int argc, char *argv[] ){
+  DIR *directory=NULL;
+  struct dirent *enter;
+  int size = 0;
+  struct stat buffer;
   if (argc > 1){
-    directory = opendir(argv[1]);
+    directory = opendir(".");
+  }
+  else{
+    printf("Enter a directory: \n");
   }
   printf("Statistics for directory: \n");
-  printf("Total Directory Size:%i Bytes \n", fsize());
+  if (directory){
+    while ((enter = readdir(directory))){
+      stat(enter->d_name, &buffer);
+	if(S_ISREG(buffer.st_mode)){
+	  printf("File: %s\n", enter->d_name);
+	  size += buffer.st_size;
+	}
+	 if(S_ISDIR(buffer.st_mode)){
+	  printf("DIRECTORY: %s \n", enter->d_name);
+	}
+    }
+    closedir(directory);
+  }
+  printf("Total Directory Size:%i Bytes \n", size);
   return 0;
 }
 
